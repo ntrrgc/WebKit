15440d15c6dc44413aa543ad83fb963f859d4057
Xabier Rodriguez-Calvar <calvaris@igalia.com>
2024-09-29 22:29:28 -0700

    [Modern Media Controls] remove window event listeners at deinitialization https://bugs.webkit.org/show_bug.cgi?id=280503
    Reviewed by Ryan Reno.
    
    DOMWindow keeps media controls object alive because of installed event listeners (see
    JSDOMWindow::visitAdditionalChildren). This results in extra memory usage undil DOMWindow cleared (on top level
    navigation).
    
    Original patch by Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>.
    
    * Source/WebCore/Modules/modern-media-controls/controls/media-controls.js:
    (MediaControls.prototype.reenable):
    * Source/WebCore/Modules/modern-media-controls/media/media-controller.js:
    (MediaController.prototype.deinitialize):
    (MediaController.prototype.reinitialize):
    
    Canonical link: https://commits.webkit.org/284440@main

--- Source/WebCore/Modules/modern-media-controls/media/media-controller.js
+++ Source/WebCore/Modules/modern-media-controls/media/media-controller.js
@@ -293,6 +293,9 @@ class MediaController
     deinitialize()
     {
         this.shadowRoot.removeChild(this.container);
+        window.removeEventListener("keydown", this);
+        if (this.controls)
+            this.controls.disable();
         return true;
     }
 
@@ -303,6 +306,9 @@ class MediaController
         this.mediaWeakRef = new WeakRef(media);
         this.host = host;
         shadowRoot.appendChild(this.container);
+        window.addEventListener("keydown", this);
+        if (this.controls)
+            this.controls.reenable();
         return true;
     }
 
